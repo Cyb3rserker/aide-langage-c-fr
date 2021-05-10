9d4d676133829b2c3bcc09e22d0457809f15ea4b
MedouneDiop <med.diop7829@gmail.com>
2021-05-10 14:37:42 +0000

    Modification du fichier pour plus de clareté dans la compréhension du système de cast

--- donnees/cast.c
+++ donnees/cast.c
@@ -1,13 +1,21 @@
 // Programme introduisant le fait de "caster" des éléments.
-// "caster" signifie mettre un élément sous une autre forme.
+// "caster" signifie changer la manière dont le programme 
+// interprète une donnée.
 
 #include <stdio.h>
 #include <cs50.h>
 
 int main ()
 {
-    char c = get_char("Donnez un caractère :  ");
+    char c = 'F';
     // On prendra pour exemple un caractère que l'on vas changer en entier.
-    printf("%i\n", (int) c);
-    // On le cast grâce à %i et (int).
+    int c2 = (int) c;
+    // On change l'interprétation du programme concernant la donnée, ce qui 
+    // permet de faire l'initialisation de la variable correctement.
+    printf("%i\n", c2);
+    // Ce qui équivaut aussi à :
+    // printf("%i\n", (int) c);
+
+    // Sortie au terminal :
+    // 70
 }
